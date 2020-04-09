01d2762226414769030369b8f1bfbf8943f9e1b9
vhngroup <info@vhngroup.com>
2020-04-09 01:08:18 -0500

    Implementaciòn de guardado de datos en fichero ,csv

--- glassdoor_scraper.py
+++ glassdoor_scraper.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 import time
 import pandas as pd
 
-def get_jobs(keyword, num_jobs, verbose, path, slp_time):
+def get_jobs(keyword, Location_Id,  num_jobs, verbose, path, slp_time):
     
     '''Gathers jobs as a dataframe, scraped from Glassdoor'''
     
@@ -17,7 +17,8 @@ def get_jobs(keyword, num_jobs, verbose, path, slp_time):
     #Change the path to where chromedriver is in your home folder.
     driver = webdriver.Chrome(executable_path=path, options=options)
     driver.set_window_size(1120, 1000)
-    url = 'https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=true&clickSource=searchBtn&typedKeyword="' + keyword + '"&sc.keyword=" '+ keyword + '"&locT=&locId=&jobType='
+    url = 'https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=true&clickSource=searchBtn&typedKeyword="' + keyword + '"&sc.keyword=" '+ keyword + '"&locT=&locId=" '+ Location_Id + '"&jobType='
+    url = 'https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=false&clickSource=searchBtn&typedKeyword=data+science&sc.keyword=data+science&locT=N&locId=54&jobType='
 
     #url = 'https://www.glassdoor.com/Job/jobs.htm?sc.keyword="' + keyword + '"&locT=C&locId=1147401&locKeyword=San%20Francisco,%20CA&jobType=all&fromAge=-1&minSalary=0&includeNoSalaryJobs=true&radius=100&cityId=-1&minRating=0.0&industryId=-1&sgocId=-1&seniorityType=all&companyId=-1&employerSizes=0&applicationType=0&remoteWorkType=0'
     driver.get(url)
--- untitled.py
+++ untitled.py
@@ -2,5 +2,6 @@ import glassdoor_scraper as gs
 import pandas as pd
 path =  "D:/Git/ds_salary_proj/chromedriver"
 
-df = gs.get_jobs('data science', 15, False, path, 15)
-df
+df = gs.get_jobs('data science', '54', 10, False, path, 15)
+
+df.to_csv('glassdoor_jobs.csv', index = False)

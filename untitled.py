9f76fad83a23a20ad1a06cf6b78fbea03acd9101
vhngroup <info@vhngroup.com>
2020-04-15 09:49:55 -0500

    Se inicia etapa de Data Analys con #jupyter

--- untitled.py
+++ untitled.py
@@ -1,23 +1,26 @@
 import glassdoor_scraper as gs
+import data_Cleaning as d_T
 import pandas as pd
 
+contry = '' #ingrese las iniciales del pais o ciudad a filtrar
+year = 2020
+
+def main():
+      inicializar()
+      extrac_Data()
+
 def inicializar():
-    path =  "D:/Git/ds_salary_proj/chromedriver"
-    df = gs.get_jobs('data science', 1000, False, path, 15)
-    df.to_csv('glassdoor_jobs.csv', index = False)
-
-def arreglo ():
-    contry = '' #ingrese las iniciales del pais o ciudad a filtrar
-    year = 2020
-    return contry, year
+   path =  "D:/Git/ds_salary_proj/chromedriver"
+   df = gs.get_jobs('data science', 200, False, path, 15)
+   df.to_csv('glassdoor_jobs.csv', index = False)
 
 
-if __name__ == "__main__": # Se inicializa el programa
-    arreglo()
-    inicializar()
-    
+def extrac_Data():
+    m_DT = d_T.load_data()
+    # return contry, year
  
-
+if __name__ == "__main__": # Se inicializa el programa
+    main()
 
 
 
